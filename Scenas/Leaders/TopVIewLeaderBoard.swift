f4f8653876fbcdf66791117a004aef4baeb5a3e0
GiorgiMachablishvili <g.machablishvili@yahoo.com>
2025-03-04 21:04:12 +0400

    create top view leader board

---
+++ Scenas/Leaders/TopVIewLeaderBoard.swift
@@ -0,0 +1,20 @@
+//
+//  TopVIewLeaderBoard.swift
+//  Betfair
+//
+//  Created by Gio's Mac on 04.03.25.
+//
+
+import UIKit
+
+class TopVIewLeaderBoard: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
